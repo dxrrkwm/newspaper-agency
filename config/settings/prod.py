d4e63a8b2183af13164b559c0f5dc294840a54cf
Vadym Sulim <ohnooq1@gmail.com>
2024-12-06 04:04:58 +0200

    configured settings for deploy, added staticfiles managing

---
+++ config/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+ALLLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": int(os.getenv("POSTGRES_PORT")),
+    }
+}
